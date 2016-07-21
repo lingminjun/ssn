aa4e15502c16c660fb1795b3ca64bfa6a5c48e96
Ling Minjun <lingminjun@lingminjundeMacBook-Pro.local>
2016-07-21 22:54:59 +0800

    fadapter初步实现

---
+++ ssn/demo/setting/DMTableAdapterController.h
@@ -0,0 +1,13 @@
+//
+//  DMTableAdapterController.h
+//  ssn
+//
+//  Created by lingminjun on 16/7/18.
+//  Copyright © 2016年 lingminjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMTableAdapterController : UITableViewController
+
+@end
---
+++ ssn/demo/setting/DMTableAdapterController.m
@@ -0,0 +1,13 @@
+//
+//  DMTableAdapterController.m
+//  ssn
+//
+//  Created by lingminjun on 16/7/18.
+//  Copyright © 2016年 lingminjun. All rights reserved.
+//
+
+#import "DMTableAdapterController.h"
+
+@implementation DMTableAdapterController
+
+@end
